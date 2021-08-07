e81a4f54812a2151733dc53ef82ddfea9b69f871
ksj1208 <seongjun8566@naver.com>
2021-08-07 16:51:04 +0900

    1. exception 핸들러 추가 2. userCode 어노테이션 추가

--- src/main/java/com/dev/devblog/grobal/exception/CommonExceptionHandler.java
+++ src/main/java/com/dev/devblog/grobal/exception/CommonExceptionHandler.java
@@ -1,17 +1,42 @@
 package com.dev.devblog.grobal.exception;
 
+import com.dev.devblog.grobal.annotation.CurrentUserCode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.NoSuchElementException;
 
-@ControllerAdvice
+@Slf4j
+@RestControllerAdvice
 public class CommonExceptionHandler {
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<String> noSuchElementException(NoSuchElementException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException exception, @CurrentUserCode Long userCode) {
+        log.error("NoSuchElement exception : " + exception.getMessage() + "::" + exception);
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException exception) {
+
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<String> illegalStateException(IllegalStateException exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<String> handleNullPointerException(NullPointerException exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
     }
 }
