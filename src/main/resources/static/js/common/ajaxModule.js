1e825d7a31c672d888ff41047a6be0d4989d3203
ksj1208 <seongjun8566@naver.com>
2021-08-29 15:03:58 +0900

    1. 메인 화면 적용 2. ajax 모듈 추가

---
+++ src/main/resources/static/js/common/ajaxModule.js
@@ -0,0 +1,136 @@
+const ajaxModule = {
+    contentType: 'application/json;charset=UTF-8',
+    dataType: 'json',
+    csrfToken: $("meta[name='_csrf']").attr("content"),
+    csrfHeader: $("meta[name='_csrf_header']").attr("content"),
+
+    get: function (_url, _parameters, _callback) {
+        if (_url != null) {
+            //5번째 파라미터로 동기화 방식을 받을 수 있다.
+            //존재 여부에 따라서 넘기지 않으면 기본 true
+            let _async = arguments[4];
+            if (_async == null) _async = true;
+            //에러 시 처리할 함수를 4번째 파라미터로 받을 수 있다.
+            let _errorMsg = arguments[3];
+            //get 요청이기 때문에 쿼리스트링으로 변환
+            let queryString = '?' + $.param(_parameters);
+
+            $.ajax({
+                type: "GET",
+                url: _url + queryString,
+                async: _async,
+                success: function (rst) {
+                    _callback(rst);
+                },
+                error: function (e) {
+                    if (_errorMsg != null)
+                        _errorMsg(e);
+                    else
+                        alert("System Error!!");
+                }
+            });
+
+        } else {
+            alert("System Error!!");
+            return false;
+        }
+    },
+
+    post: function (_url, _parameters, _callback) {
+        if (_url != null) {
+            let _errorMsg = arguments[3];
+            let _async = arguments[4];
+
+            if (_async == null) _async = true;
+
+            $.ajax({
+                type: "POST",
+                url: _url,
+                data: JSON.stringify(_parameters),
+                async: _async,
+                contentType: this.contentType,
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader(ajaxModule.csrfHeader, ajaxModule.csrfHeader);
+                },
+                success: function (rst) {
+                    _callback(rst);
+                },
+                error: function (e) {
+                    if (_errorMsg != null)
+                        _errorMsg(e);
+                    else
+                        alert("System Error!!");
+                }
+            });
+
+        } else {
+            alert("System Error!!");
+            return false;
+        }
+    },
+
+    patch: function (_url, _parameters, _callback) {
+        if (_url != null) {
+            let _errorMsg = arguments[3];
+            let _async = arguments[4];
+
+            if (_async == null) _async = true;
+
+            $.ajax({
+                type: "PATCH",
+                url: _url,
+                data: JSON.stringify(_parameters),
+                async: _async,
+                contentType: this.contentType,
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader(ajaxModule.csrfHeader, ajaxModule.csrfHeader);
+                },
+                success: function (rst) {
+                    _callback(rst);
+                },
+                error: function (e) {
+                    if (_errorMsg != null)
+                        _errorMsg(e);
+                    else
+                        alert("System Error!!");
+                }
+            });
+        } else {
+            alert("System Error!!");
+            return false;
+        }
+    },
+
+    delete: function (_url, _parameters, _callback) {
+        if (_url != null) {
+            let _errorMsg = arguments[3];
+            let _async = arguments[4];
+
+            if (_async == null) _async = true;
+
+            $.ajax({
+                type: "DELETE",
+                url: _url,
+                data: JSON.stringify(_parameters),
+                async: _async,
+                contentType: this.contentType,
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader(ajaxModule.csrfHeader, ajaxModule.csrfHeader);
+                },
+                success: function (rst) {
+                    _callback(rst);
+                },
+                error: function (e) {
+                    if (_errorMsg != null)
+                        _errorMsg(e);
+                    else
+                        alert("System Error!!");
+                }
+            });
+
+        } else {
+            alert("System Error!!");
+            return false;
+        }
+    }
+}
--- src/test/java/com/dev/devblog/SecurityTest.java
+++ src/test/java/com/dev/devblog/SecurityTest.java
@@ -10,8 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ControllerTargetTest
 public class SecurityTest {
@@ -26,16 +26,18 @@ public class SecurityTest {
     @BeforeEach
     void setup() {
         //TODO 테스트 코드를 실행 전 미리 동작하는 부분 (DB에 기본정보를 넣고 시작해야하는 경우)
+        //외부라이브러리 사용이나 보안적으로 존재하는 필수데이터
         user = userDummyProvider.save();
     }
 
     @DisplayName("security 로그인 인증 테스트 - 로그인 성공 프로세스 테스트")
     @Test
     public void securityLoginSuccessTest() throws Exception {
+        //form 전송
         mockMvc.perform(post("/login")
                 .param("username", user.getUserId())
                 .param("password", "test!@")
                 .with(csrf()))
-                .andExpect(authenticated());
+                .andExpect(status().isOk());
     }
 }
